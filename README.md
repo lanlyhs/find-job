f3667ae1e85e0a170a1796593d55fa0a0b4e2d67
Lanlyhs <ziqiang.chen@outlook.com>
2020-04-16 09:02:40 +0800

    refact: don't user global variable

---
+++ README.md
@@ -0,0 +1,11 @@
+# Find Job
+
+Crawlers ideas inspire [ECommerceCrawlers](https://github.com/DropsDevopsOrg/ECommerceCrawlers)
+
+## TODO
+
+- [ ] Support Crawlers Job Details
+- [ ] Support blacklist
+- [ ] Support Multi Query Keyworkd
+- [ ] Support Other Job Website
+- [ ] Support Web / Email
--- index.js
+++ index.js
@@ -1,57 +1,60 @@
-'use strict'
+"use strict";
 
 const fs = require("fs").promises;
-
 const axios = require("axios");
-const faker = require('faker');
-const parse = require('node-html-parser').parse;
-
-const utils = require('./utils')
-
+const faker = require("faker");
 faker.locale = "zh_CN";
+const parse = require("node-html-parser").parse;
+const utils = require("./utils");
 
-const defaultQueryCity = "杭州"
-const defaultKeyword = "golang"
-const citySavePath = "city.json"
-let cityCode;
-let jobs = []
+require('dotenv').config()
+
+const citySavePath = "city.json";
 
 async function getCity() {
-    let res = await axios.get('https://www.zhipin.com/wapi/zpCommon/data/city.json');
-    await fs.writeFile(citySavePath, JSON.stringify(res.data), 'utf8');
+    let res = await axios.get(
+        "https://www.zhipin.com/wapi/zpCommon/data/city.json"
+    );
+    await fs.writeFile(citySavePath, JSON.stringify(res.data), "utf8");
 }
 
 async function getCityCode() {
-    await getCity()
 
-    let city = await fs.readFile(citySavePath, 'utf8')
-    city = JSON.parse(city)
-    let hotCityList = city.zpData.hotCityList
+    await getCity();
 
+    let city = await fs.readFile(citySavePath, "utf8");
+    city = JSON.parse(city);
+    let hotCityList = city.zpData.hotCityList;
+
+    let cityCode;
     for (let city of hotCityList) {
-        if (city.name === defaultQueryCity) {
+        if (city.name === process.env.QUERY_CITY) {
             cityCode = city.code;
             break;
         }
     }
     if (!cityCode) {
-        console.log("don't find city code")
-        process.exit()
+        console.error("don't find city code");
+        process.exit();
     }
+    return cityCode;
 }
 
 async function getCityJob() {
-    let page = 1
-    while(true) {
-        let res = await axios.get('https://www.zhipin.com/mobile/jobs.json', {
+    let cityCode = await getCityCode();
+
+    let jobs = [];
+    let page = 1;
+    while (true) {
+        let res = await axios.get("https://www.zhipin.com/mobile/jobs.json", {
             headers: {
-                'User-Agent': faker.internet.userAgent()
+                "User-Agent": faker.internet.userAgent(),
             },
             params: {
-                'query': defaultKeyword,
-                'page' : page,
-                'city': cityCode
-            }
+                query: process.env.QUERY_KEYWORD,
+                page: page,
+                city: cityCode,
+            },
         });
 
         let data = JSON.parse(JSON.stringify(res.data));
@@ -60,25 +63,27 @@ async function getCityJob() {
             break;
         }
 
-        const root = parse(code)
-        for (let job of root.querySelectorAll('li.item')) {
+        const root = parse(code);
+        for (let job of root.querySelectorAll("li.item")) {
             jobs.push({
-                "职位名称": job.querySelector('div.title h4').text,
-                "职位链接": "https://www.zhipin.com"+job.querySelector('a').getAttribute('href'),
-                "公司名称": job.querySelector('div.name').text,
-                "薪资": job.querySelector('div.title .salary').text,
+                职位名称: job.querySelector("div.title h4").text,
+                职位链接:
+                    "https://www.zhipin.com" +
+                    job.querySelector("a").getAttribute("href"),
+                公司名称: job.querySelector("div.name").text,
+                薪资: job.querySelector("div.title .salary").text,
                 // "工作地点": job.querySelectorAll('div.msg em')[0].text,
-                "工作经验": job.querySelectorAll('div.msg em')[1].text,
-                "学历要求": job.querySelectorAll('div.msg em')[2].text,
-            })
+                工作经验: job.querySelectorAll("div.msg em")[1].text,
+                学历要求: job.querySelectorAll("div.msg em")[2].text,
+            });
         }
         page++;
-        utils.msleep(200)
+        utils.msleep(200);
     }
+    return jobs;
 }
 
 (async function () {
-    await getCityCode()
-    await getCityJob()
-    console.table(jobs)
-})()
+    let jobs = await getCityJob();
+    console.table(jobs);
+})();
